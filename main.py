8c3517177c729a1f15c07ab292f92ad6501e2647
Las02 <lasse101010@gmail.com>
2022-10-27 20:24:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
+"""
+real    1m52.000s
+user    1m46.734s
+sys     0m2.438s
+
+"""
+
 import sys
 import re
 import gzip
@@ -20,27 +27,48 @@ def FindKmer(dna, kmer_len):
 
     return kmer_list
 
-def CountEachKmer(kmer_list, DataStructure):
+def CountEachKmer(kmer_list, Data_Structure):
     '''Counts the amount of each kmer in the dict: kmer_count'''
     for kmer in kmer_list:
-        if kmer in DataStructure:
-            DataStructure[kmer]["count"] += 1
+        if kmer in Data_Structure:
+            Data_Structure[kmer]["count"] += 1
 
-def CountKmerPerAR(nKmer_per_AR, DataStructure):
+def CountKmerPerAR(nKmer_per_AR, Data_Structure):
     '''Counts the amount of all kmers for each AntibioticResistence Gene in the dict: nKmer_per_AR'''
 
     # Goes trough each kmer
-    for data in DataStructure.values():
+    for data in Data_Structure.values():
 
         # Go through each AR_gene for which the kmer was found and add the count to it
         for AR_gene in data["AR_genes"]:
-            nKmer_per_AR[AR_gene] += data["count"]
+            
+            if data["count"] == 0:
+                pass
+
+            elif AR_gene in nKmer_per_AR:
+                nKmer_per_AR[AR_gene] += data["count"]
+            
+            else:
+                nKmer_per_AR[AR_gene] = data["count"]
+                print("HEEEE")
+
+
+def AddToDatastructure(Data_Structure, kmer_list, header):
+    """Adds the kmer_list and header to the Data_structure """
+    for kmer in kmer_list:
+        # If the kmer is allready assigned to an AR gene, add the additional
+        if kmer in Data_Structure:
+            Data_Structure[kmer]["AR_genes"].add(header)
+        # Else add the kmer to the datastructure
+        else:
+            Data_Structure[kmer] = {"count":0, "AR_genes":{header}}
+
                 
 
 # Set the kmer lenght to look for
 kmer_length = 5
-# Store which Antibiotic Resistence (AR) gene has which kmers
-DataStructure = dict()
+# Stores {Kmer: {"count":0,{"AR":}}}, The Kmer as key, and then both the number of time it is seen, and the AR genes which has the kmer
+Data_Structure = dict()
 Could_be_kmers = dict()
 nKmer_per_AR = dict()
 
@@ -48,7 +76,6 @@ nKmer_per_AR = dict()
 
 AR_file = open('resistance_genes.fsa.txt', 'r')
 filename = "Unknown3_raw_reads_1.txt.gz"
-
 #AR_file = open('ARsmall.txt', 'r')
 #filename = "smallfastaseq.txt.gz"
 
@@ -76,20 +103,8 @@ while line != '':
     # Finding all the posible kmers
     kmer_list = FindKmer(dna, kmer_length)
 
-    #For making the total count later
-    if header not in nKmer_per_AR:
-        nKmer_per_AR[header] = 0
-
-    # TODO make as function
-    for kmer in kmer_list:
-        # If the kmer is allready assigned to an AR gene, add the additional
-        if kmer in DataStructure:
-            DataStructure[kmer]["AR_genes"].add(header)
-        # Else add the kmer to the datastructure
-        else:
-            DataStructure[kmer] = {"count":0, "AR_genes":{header}}
-
-
+    # Adding the kmer_list and header to the Data_structure 
+    AddToDatastructure(Data_Structure, kmer_list, header)
 
 
 ## Reading in the sequenceing file
@@ -109,12 +124,12 @@ for line in sample_file:
         # Find all posible kmers
         kmer_list = FindKmer(dna, kmer_length)
         # Count the foind posible kmers
-        CountEachKmer(kmer_list, DataStructure)
+        CountEachKmer(kmer_list, Data_Structure)
         
     last_line = line
     
 
-CountKmerPerAR(nKmer_per_AR, DataStructure)
+CountKmerPerAR(nKmer_per_AR, Data_Structure)
 
 print(nKmer_per_AR)
 
