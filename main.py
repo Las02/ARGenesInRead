ac78bfd8a21df4522258e2acd670f40523f305d9
Las02 <lasse101010@gmail.com>
2022-10-27 09:46:08 +0200

    Read in file

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import gzip
 
+def CompareDNA(dna):
+    print(dna)
 
-print("hello world!")
-print("ok")
-print("hello")
-hej
+filename = "Unknown3_raw_reads_1.txt.gz"
+sample_file = gzip.open(filename, "r")
+
+in_dna = False
+for line in sample_file:
+    line = line.decode("utf-8")
+    
+    if in_dna:
+        dna = line.strip()
+        CompareDNA(dna)
+        in_dna = False
+
+    if line.startswith("@"):
+        in_dna = True
+    
+    
