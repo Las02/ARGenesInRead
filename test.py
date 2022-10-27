b75486dc4b92df89a28eabea5612cd678d0e15e0
Las02 <lasse101010@gmail.com>
2022-10-27 11:14:14 +0200

    fixed countsutff

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
 
+from findkmer import FindKmer
 
+def Count(Kmerlist,KmCount,header,ARCount):
+    for Kmer in Kmerlist:
+        
+        if Kmer in KmCount:
+            KmCount[Kmer] += 1
+        else:
+            KmCount[Kmer] = 1
+            
+    ARCount[header] = Kmerlist
+
+Kmerlist = FindKmer("ATCGY", 3)
+KmCount = dict()
+ARCount = dict()
+header = "AR"
+Count(Kmerlist,KmCount,header,ARCount)
+
+
+#print(Kmerlist)
