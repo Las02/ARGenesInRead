d529a46c8620057a09ed16443cee4768de103953
Las02 <lasse101010@gmail.com>
2022-11-11 11:15:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,38 +57,35 @@ def FindKmer(dna, kmer_len):
 
     return kmer_list, range_list
 
-def CountEachKmer(kmer_list, gene_data, nKmer_per_AR):
-    '''Counts the amount of each kmer in the dict: kmer_count'''
+def CountEachKmer(kmer, gene_data, found_kmer_in_gene,kmer_length):
+    '''if input kmer is equal to kmer in gene_data 
+       make array with depht and store in found_kmer_in_gene'''
 
-    # Goes through each found kmer
-    for kmer in kmer_list:
-        # If the kmer is equal to an AR gene
-        if kmer in gene_data:
-            AR_to_range = gene_data[kmer]
-    
-            # Go through each AR_gene for which the kmer was found and add the count to it
-            for AR_gene in AR_to_range:
-                
-                if AR_gene in nKmer_per_AR:
-                    from_to_len = AR_to_range[AR_gene]
-                    AddDepth(from_to_len, nKmer_per_AR, AR_gene)
-                
-                else:
-                    # Extract [from, to, len(dna)]
-                    from_to_len = AR_to_range[AR_gene]
-                    # Make vector of [0] to represent depht of each nt
-                    length_of_gene = from_to_len[1]
-                    nKmer_per_AR[AR_gene] = [0] * length_of_gene
+    def AddDepth(kmer_pos, found_kmer_in_gene, gene,kmer_length):
+        """Add the count for the kmer to position from which it was found"""
+        from_range = kmer_pos
+        to_range = kmer_pos + kmer_length     
+        for i in range(from_range, to_range):
+            found_kmer_in_gene[gene][i] = 1
 
-                    # Add the count for the kmer to the specific place
-                    AddDepth(from_to_len, nKmer_per_AR, AR_gene)
-            
-def AddDepth(from_to_len, nKmer_per_AR, AR_gene):
-    """Add the count for the kmer to the specific place"""
-    from_range = from_to_len[0]
-    to_range = from_range + kmer_length
-    for i in range(from_range, to_range):
-        nKmer_per_AR[AR_gene][i] = 1
+    # If the kmer is equal to a kmer in the genes
+    if kmer in gene_data:
+        # Go through each gene which has the kmer and add depht to it.
+        for genename in gene_data[kmer]:
+            len_gene = gene_data[kmer][genename][1]
+            kmer_pos = gene_data[kmer][genename][0]
+
+            if genename in found_kmer_in_gene:
+                # Add depht to it, corresponding the kmer found
+                AddDepth(kmer_pos, found_kmer_in_gene, genename, kmer_length)
+            else:
+                # Make vector of [0] to represent depht of each nt corresponding to length of gene
+                found_kmer_in_gene[genename] = [0] * len_gene
+
+                # Add depht to it, corresponding the kmer found
+                AddDepth(kmer_pos, found_kmer_in_gene, genename ,kmer_length)
+        
+
 
 def AddToDatastructure(gene_data, kmer_list, header, range_list):
     """Adds the kmer_list and header to the gene_data """
@@ -189,11 +186,12 @@ for dna in read_qfasta(read_filename):
     # Find all posible kmers
     (kmer_list, range_list) = FindKmer(dna, kmer_length)
     # Count the found posible kmers
-    nKmer_per_AR = dict()
-    CountEachKmer(kmer_list, gene_data, nKmer_per_AR)
+    found_kmer_in_gene = dict()
+    for kmer in kmer_list:
+        CountEachKmer(kmer, gene_data, found_kmer_in_gene,kmer_length)
 
     # Do some check with it
-    for genename,gene in nKmer_per_AR.items():
+    for genename,gene in found_kmer_in_gene.items():
         if judge(gene, dna):
             # Add den til final count
             if genename in gene_count:
@@ -201,7 +199,7 @@ for dna in read_qfasta(read_filename):
                     gene_count[genename][i] += gene[i]    # Skal plusse element pr element  
             else :
                 gene_count[genename] = gene 
-
+    print(found_kmer_in_gene)
 
 coverage_depht = dict()
 for genename, dna in gene_count.items():
