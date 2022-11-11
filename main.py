5a7a45d8692819f9fa4903ed794586be288ef530
Las02 <lasse101010@gmail.com>
2022-11-11 10:40:30 +0100

    dsd

--- main.py
+++ main.py
@@ -23,20 +23,19 @@ def read_fasta(filename):
 def read_qfasta(filename):
     '''Extract the dna from a gzippedfastaQ file '''
 
-
     sample_file = gzip.open(filename, "r")
     
     last_line = ""
     for line in sample_file:
         line = line.decode("utf-8") 
 
-        # Extracting only the dna
-        # TODO hver 110% sikre på at den ikke kan være på 2 linjer
+        # Extracting the dna
         if last_line.startswith("@"):
             dna = line.strip()
             yield dna
                     
         last_line = line
+
     sample_file.close()
 
 def FindKmer(dna, kmer_len):
@@ -58,14 +57,14 @@ def FindKmer(dna, kmer_len):
 
     return kmer_list, range_list
 
-def CountEachKmer(kmer_list, Data_Structure, nKmer_per_AR):
+def CountEachKmer(kmer_list, gene_data, nKmer_per_AR):
     '''Counts the amount of each kmer in the dict: kmer_count'''
 
     # Goes through each found kmer
     for kmer in kmer_list:
         # If the kmer is equal to an AR gene
-        if kmer in Data_Structure:
-            AR_to_range = Data_Structure[kmer]
+        if kmer in gene_data:
+            AR_to_range = gene_data[kmer]
     
             # Go through each AR_gene for which the kmer was found and add the count to it
             for AR_gene in AR_to_range:
@@ -91,18 +90,18 @@ def AddDepth(from_to_len, nKmer_per_AR, AR_gene):
     for i in range(from_range, to_range):
         nKmer_per_AR[AR_gene][i] = 1
 
-def AddToDatastructure(Data_Structure, kmer_list, header, range_list):
-    """Adds the kmer_list and header to the Data_structure """
+def AddToDatastructure(gene_data, kmer_list, header, range_list):
+    """Adds the kmer_list and header to the gene_data """
     for i in range(len(kmer_list)):
         kmer = kmer_list[i]
         kmer_range = range_list[i]
 
         # If the kmer is allready assigned to an AR gene, add the additional
-        if kmer in Data_Structure:
-            Data_Structure[kmer][header] = (kmer_range)
+        if kmer in gene_data:
+            gene_data[kmer][header] = (kmer_range)
         # Else add the kmer to the datastructure
         else:
-            Data_Structure[kmer] = {header:kmer_range}
+            gene_data[kmer] = {header:kmer_range}
 
 def find_coverage_depht(dna):
 
@@ -163,7 +162,7 @@ kmer_length = 4
 
 # Stores {Kmer: {"AR":kmer_range}}, The Kmer as key. 
 #then there is a inner dict with the AR gene as key and the kmer_ range as values
-Data_Structure = dict()
+gene_data = dict()
 Could_be_kmers = dict()
 
 #filename = "Unknown3_raw_reads_1.txt.gz"
@@ -175,8 +174,8 @@ for dna, header in read_fasta(gene_filename):
     # Finding all the posible kmers and its positions
     (kmer_list, range_list) = FindKmer(dna, kmer_length)
 
-    # Adding the kmer_list and header to the Data_structure 
-    AddToDatastructure(Data_Structure, kmer_list, header, range_list)
+    # Adding the kmer_list and header to the gene_data 
+    AddToDatastructure(gene_data, kmer_list, header, range_list)
 
 
 
@@ -191,7 +190,7 @@ for dna in read_qfasta(read_filename):
     (kmer_list, range_list) = FindKmer(dna, kmer_length)
     # Count the found posible kmers
     nKmer_per_AR = dict()
-    CountEachKmer(kmer_list, Data_Structure, nKmer_per_AR)
+    CountEachKmer(kmer_list, gene_data, nKmer_per_AR)
 
     # Do some check with it
     for genename,gene in nKmer_per_AR.items():
