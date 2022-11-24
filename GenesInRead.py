a94d39c89bfb1b60daa29da9b90bc90bdb165b8e
Las02 <lasse101010@gmail.com>
2022-11-24 11:52:59 +0100

    ss

--- GenesInRead.py
+++ GenesInRead.py
@@ -2,7 +2,7 @@
 import sys
 import gzip
 
-def argument_parser(argv,kmer_length, gene_file, read_file):
+def argument_parser(argv, kmer_length, gene_file, read_file):
     """
     Sets up a argument parser from the argv vector. 
     In addition takes in default values for variables, if not given 
@@ -43,13 +43,13 @@ def read_fasta(filename):
         sys.exit(f"The file '{filename}' could not be found, error: {errormessage}")
 
     # Extract the dna, and the headers.
-    oldheader = "firstheader"
+    oldheader = "FIRST HEADER"
     for line in file:
         line = line.strip()
-        #if line is header yield dna and header except first header
+        #if line is header yield dna and header except FIRST HEADER
         if line.startswith(">"):    
             newheader = line
-            if oldheader != "firstheader":
+            if oldheader != "FIRST HEADER":
                 yield dna, oldheader
             dna = ""
             oldheader = newheader
@@ -59,8 +59,8 @@ def read_fasta(filename):
     yield dna, oldheader
     file.close()
     
-def read_qfasta(filename):
-    '''Extract dna from a gzipped fastaQ file '''
+def read_fastq(filename):
+    '''Extract dna from a gzipped fastQ file '''
     # Try to open the file, error if file does not exsist
     try: sample_file = gzip.open(filename, "r")
     except FileNotFoundError as errormessage:
@@ -81,101 +81,88 @@ def read_qfasta(filename):
     # Error message if the gzipped file does not work
     except gzip.BadGzipFile as errormessage:
         sys.exit(f"The file '{filename}' could not be gzipped, error: {errormessage}")
-
     sample_file.close()
 
-def find_all_kmers(dna, kmer_len):
+def get_kmers_and_pos(dna, kmer_len):
     '''Find Kmers from a dna string and return list with them
-       in additon to list with their positions
+       in additon to list with their positions in the dna string
        '''
-
-    # Return None if there is no possible kmer's
+    # Return empty lists if there is no possible kmer's
     if kmer_len > len(dna):
-        return
+        return [],[]
 
+    #  Find all possible kmers
     kmer_list = []
     range_list = []
-    from_range = 0
     to_range = len(dna) - kmer_len + 1   #+1 to also add last kmer
-    for i in range(from_range, to_range):
-
+    for i in range(0, to_range):
         kmer = dna[i: i + kmer_len]
         kmer_list.append(kmer)
         range_list.append(i)
 
     return kmer_list, range_list
 
-def read_is_valid(gene, read):
+def read_is_valid(depht_count, dna_read):
     '''
     return True if the read is covering enough of the gene,
     else returns False
-    Running time: O(len(gene))
+    The input is a list of ints representing the depth of each nucleotide. 
     '''
-    # Various parameters
-    max_space = 1   # The maximum space in the read covering the gene
-    side_bonus = int(0.70 * len(read))   # How much of the read can be outside the gene
-    threshold_score = int(0.95 *len(read))   #The percent of the read which needs to cover the gene
+    # Set parameters
+    MAX_SPACE = 1   # The maximum space allowed in the read covering the gene
+    SIDE_BONUS = int(0.70 * len(dna_read))   # How much of the read can be outside the gene and still be considered valid
+    THRESHOLD_SCORE = int(0.95 *len(dna_read))   # The percent of the read which needs to cover the gene
 
-    # Various init
-    maxcount = 0
-    count = 0
-    exitcount = 0
-    in_kmer = False
-    len_dna = len(gene)
-
-    # Score the reads coverage of the gene
-    for pos,value in enumerate(gene):
-
-        # Start counting the coverage at first [1] in gene
-        if value == 1: in_kmer = True
-        if in_kmer:
-            if value == 0:
-                exitcount += 1
-            else:
-                count += 1
-                # If at either side position, add a bonus to the score
-                if pos in [0, len_dna-1]:
-                    count += side_bonus
-
-            # If a better fitting coverage of the read to the gene is found, use it
-            if count > maxcount:
-                maxcount = count
-            
-            # Stop counting the coverage if there is too much spacing
-            if exitcount >= max_space:
-                count = 0
+    # Find the reads largest continuous coverage of the gene.
+    # a coverage is considered continuous if the spacing does not exceed the MAX_SPACE 
+    END_OF_GENE = len(depht_count) -1
+    (maxcount, score, exitcount) = (0,0,0)
+    in_coverage = False
+    for pos,value in enumerate(depht_count):
+        # Start counting the coverage when a depht != 0
+        if value == 1: in_coverage = True
+        # Start counting the coverage
+        if in_coverage:
+            # If a position is not covered, start counting to exit
+            if value == 0: exitcount += 1
+            # Else add to the coverage score for each position
+            else: score += 1
+            # If at either side position of the gene, add a bonus to the score
+            # The positions are one step from either ends to still get SIDE_BONUS when pos 0 is not covered but pos 1 is.
+            if pos in [0+1, END_OF_GENE -1]:  
+                score += SIDE_BONUS
+            # update the maxscore for the read
+            if score > maxcount: maxcount = score
+            # Stop counting the coverage if spacing exceeds MAX_SPACE
+            if exitcount >= MAX_SPACE:
+                score = 0
                 exitcount = 0
-                in_kmer = False
+                in_coverage = False
 
-    return maxcount >= threshold_score
+    return maxcount >= THRESHOLD_SCORE
 
-def coverage_stats(dna):
+def coverage_stats(depht_count):
     '''from depht array return coverage, avg depht and min_depht
-        Running time: O(len(dna))
     '''
 
-    # Init
+    # Find total_depht, coverage and avg_depht
     count = 0
     total_depht = 0
     min_depth = 99999
-
-    # Find total_depht, coverage and avg_depht
-    for base in dna:
+    for base in depht_count:
         total_depht += base
         if base != 0:
             count += 1
         if base < min_depth:
             min_depth = base
-    coverage = count / len(dna)
-    avg_depht = total_depht / len(dna)
+    coverage = count / len(depht_count)
+    avg_depht = total_depht / len(depht_count)
     return coverage, avg_depht, min_depth
 
-
 # Setting up the argument parser
-(kmer_length,gene_filename,read_filename) = argument_parser(sys.argv,19,"resistance_genes.fsa","Unknown3_raw_reads_1.txt.gz")
+(kmer_length,gene_filename,read_filename) = argument_parser(sys.argv, 19, "resistance_genes.fsa", "Unknown3_raw_reads_1.txt.gz")
 
-
-### Read in the file with the antibiotic resistence genes and save it in the datastructure kmer2gene2kmerpos
+# Read in the file with the antibiotic resistence genes and save it in the datastructure kmer2gene2kmerpos
 kmer2gene2kmerpos = dict()
 trans = str.maketrans("ACTG","TGAC")
 for non_complement_dna, header in read_fasta(gene_filename):
@@ -183,7 +170,7 @@ for non_complement_dna, header in read_fasta(gene_filename):
     # MÅSKE OGSÅ SKAL VÆRE DEN ANDEN VEJ?
     complement_dna= non_complement_dna.translate(trans)
     for dna in [non_complement_dna, complement_dna,non_complement_dna[::-1], complement_dna[::-1]]:
-        (kmer_list, kmer_positions) = find_all_kmers(dna, kmer_length)
+        (kmer_list, kmer_positions) = get_kmers_and_pos(dna, kmer_length)
         # Make gene_data datastructure which consists of: {Kmer: {"gene_name":(kmer_position_in_gene,length_gene)}}
         for kmer, kmer_pos_in_gene in zip(kmer_list, kmer_positions):
             if kmer in kmer2gene2kmerpos:
@@ -194,9 +181,9 @@ for non_complement_dna, header in read_fasta(gene_filename):
 ### Reading in the fastaq file, for each read evaluate if read is valid.
 ### If valid, add it to total depht for each gene
 TOTALgene2depht_count = dict()
-for dna_read in read_qfasta(read_filename):
+for dna_read in read_fastq(read_filename):
 
-    (kmer_list, _) = find_all_kmers(dna_read, kmer_length)
+    (kmer_list, _) = get_kmers_and_pos(dna_read, kmer_length)
 
     READgene2depht_count = dict()
     for kmer in kmer_list:
